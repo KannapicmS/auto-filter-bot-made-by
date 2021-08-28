fc6bbe7ae9195f14db85ea274cce41a5058e115c
KannapicmS <88262676+KannapicmS@users.noreply.github.com>
2021-08-28 07:00:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -88,15 +88,8 @@ async def answer(bot, query):
 
 
 def get_reply_markup(query):
-    buttons = [
-        [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
-        ],
-        [
-            InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
-        ]
-        ]
-    return InlineKeyboardMarkup(buttons)
+    button = [InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)],
+    return InlineKeyboardMarkup(button)
 
 
 def get_size(size):
